682aed7d443814c8adb0f22990fa5dd5b8629a75
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-01-20 14:50:38 +0100

    Allow showing and closing dialog element

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,4 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	});
+
+	// Show and close the modal dialog
+
+	const dialog = document.querySelector("#gfl-dialog");
+	const showButton = document.querySelector("#gfl-dialog-show");
+	const closeButton = document.querySelector("#gfl-dialog-close");
+
+	// "Show the dialog" button opens the dialog modally
+	showButton.addEventListener("click", () => {
+		dialog.showModal();
+	});
+
+	// "Close" button closes the dialog
+	closeButton.addEventListener("click", () => {
+		dialog.close();
+	});
 });
